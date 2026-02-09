43c39bacd299dd6aec6c231fd2741d56f139db18
m-mujtabaaa <mmkww2007@gmail.com>
2026-02-09 02:46:15 -0800

    Initial commit

--- src/assets/components/Curser.jsx
+++ src/assets/components/Curser.jsx
@@ -31,7 +31,7 @@ const Curser = () => {
     };
   }, []);
 
-  return <div className="cursor hidden md:block" ref={cursorRef}></div>
+  return <div className="cursor" ref={cursorRef}></div>
 ;
 };
 
--- src/assets/components/Profile.jsx
+++ src/assets/components/Profile.jsx
@@ -60,7 +60,7 @@ function Profile() {
 
         {/* Sidebar */}
         <aside className={`bg-[#075197] text-white p-6  
-        fixed top-0 left-0 h-full z-50 
+        fixed top-0 left-0 h-full md:h-auto z-50 
         w-64 transform transition-transform duration-300
         ${open ? "translate-x-0" : "-translate-x-full"} 
         md:translate-x-0 md:static md:w-[20%]`}
